6b17bb5de71f3db7bf09f2c9e284a8e973f923c5
Ivan Pryvalov <ivan.pryvalov@gmail.com>
2016-07-02 11:39:53 +0200

    Fixes #2. Fetch 100k of text takes less than 1 sec.

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
-function extract(element, el_class, attribute, where){
+function extract(el_class, attribute, where){
   var text = "";
-  $(element+'[class~="'+el_class+'"]', where).each(function(){
+  $('.'+el_class, where).each(function(){
     if (attribute){
       text = $(this).attr(attribute);       
     }else{    
@@ -18,24 +18,26 @@ function lead(a){
 }
 
 function extractHistory(html_str){  
+  var timeStart = new Date()
   var parser = new DOMParser();
   var htmlDoc = parser.parseFromString(html_str, "text/html");
   var jdoc = $(htmlDoc);
   var textArea = "Your Telegram History\n";
   var curDate = null;
   var curDateFormatted = null;
-  $('div[class~="im_history_messages_peer"]', jdoc).not('[class~="ng-hide"]').each(function() {
-    $('div[class~="im_history_message_wrap"]', $(this)).each(function() {
+  $('.im_history_messages_peer', jdoc).not('.ng-hide').each(function() {
+    //$('div[class~="im_history_message_wrap"]', $(this)).each(function() {
+    $('.im_history_message_wrap', $(this)).each(function() {
       //extract current date split
-      $('div[class~="im_message_date_split"]', $(this)).each(function(){
-        $('span[class~="im_message_date_split_text"]', $(this)).each(function(){
+      $('.im_message_date_split', $(this)).each(function(){
+        $('.im_message_date_split_text', $(this)).each(function(){
           curDate = $(this).text();
         });
       });
       //proceed with messages
-      var text = extract('div','im_message_text', null, $(this));
-      var author = extract('a','im_message_author', null, $(this));
-      var time = extract('span','im_message_date', 'data-content', $(this));  
+      var text = extract('im_message_text', null, $(this));
+      var author = extract('im_message_author', null, $(this));
+      var time = extract('im_message_date', 'data-content', $(this));  
       var fullDateStr = curDate + ", " + time;
       var d = new Date(Date.parse(fullDateStr));
       curDateFormatted = lead(d.getDate())+'.'+lead(d.getMonth()+1)+'.'+d.getFullYear() + ' ' + lead(d.getHours()) + ':' + lead(d.getMinutes()) + ':' + lead(d.getSeconds());    
@@ -43,6 +45,11 @@ function extractHistory(html_str){
     });
   });  
   $('#myTextarea').val(textArea);
+  var elapsedTime = new Date()-timeStart;
+  var logMsg = 'Working time: '+elapsedTime+' ms.' + ' Size ' + textArea.length;
+  console.log(logMsg);
+  $('.classOne').val(logMsg);
+  //$('#myTextAreaTest').val(logMsg);
 }
 
 document.addEventListener('DOMContentLoaded', function() {
